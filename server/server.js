06404033b561b29949173ea6ba132a9b52368018
dishupandey57 <dishupandey57@gmail.com>
2024-08-11 10:46:36 +0000

    credentials update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = 5555;
 const apiKey = process.env.GEMINI_API_KEY
-const credentails = 'server/Credentials/lofty-block-415112-dc2cce1356ff.json'
+const credentails = 'credentials/cred.json'
 
 // Use CORS middleware to allow requests from different origins
 app.use(cors({
